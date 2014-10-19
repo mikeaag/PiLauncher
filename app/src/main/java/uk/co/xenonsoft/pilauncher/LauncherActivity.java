f528fc534f8773a1009aaa713745438283a7fe3a
Mike Gatward <mike.gatward@xenonsoft.co.uk>
2014-10-20 00:51:36 +0100

    Add basic UI and test messages for launching fireworks

--- app/src/main/java/uk/co/xenonsoft/pilauncher/LauncherActivity.java
+++ app/src/main/java/uk/co/xenonsoft/pilauncher/LauncherActivity.java
@@ -1,16 +1,16 @@
 package uk.co.xenonsoft.pilauncher;
 
 import android.app.Activity;
-import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-
+import android.widget.Switch;
 
 
 public class LauncherActivity extends Activity {
@@ -46,6 +46,121 @@ public class LauncherActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onArmLauncherClicked(View view) {
+        Switch s = (Switch) view;
+        boolean isArmed = s.isChecked();
+
+        // Disarm until confirmation
+        disarmLauncher(s);
+
+        if (isArmed) {
+            showConfirmArmDialog(s);
+        }
+    }
+
+    public void armLauncher(Switch s) {
+        s.setChecked(true);
+
+    }
+
+    public void disarmLauncher(Switch s) {
+        s.setChecked(false);
+    }
+
+    public void showConfirmArmDialog(final Switch s) {
+        new AlertDialog.Builder(this)
+                .setTitle("Arm Launcher?")
+                .setMessage("Are you sure you want to arm the launcher?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        armLauncher(s);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        disarmLauncher(s);
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    public void onLaunchFirework(View view) {
+        Integer FireworkNumber = 0;
+        switch (view.getId()) {
+            case R.id.btnLaunchFirework1:
+                FireworkNumber = 1;
+                break;
+
+            case R.id.btnLaunchFirework2:
+                FireworkNumber = 2;
+                break;
+
+            case R.id.btnLaunchFirework3:
+                FireworkNumber = 3;
+                break;
+
+            case R.id.btnLaunchFirework4:
+                FireworkNumber = 4;
+                break;
+
+            case R.id.btnLaunchFirework5:
+                FireworkNumber = 5;
+                break;
+
+            case R.id.btnLaunchFirework6:
+                FireworkNumber = 6;
+                break;
+
+            case R.id.btnLaunchFirework7:
+                FireworkNumber = 7;
+                break;
+
+            case R.id.btnLaunchFirework8:
+                FireworkNumber = 8;
+                break;
+        }
+
+        View armSwitch = findViewById(R.id.armSwitch);
+        boolean isArmed = ((Switch) armSwitch).isChecked();
+
+        if (isArmed) {
+            launchFireworkMessage(FireworkNumber);
+        } else {
+            armLauncherFirstMessage();
+        }
+    }
+
+    public void launchFireworkMessage(Integer FireworkNumber) {
+        new AlertDialog.Builder(this)
+                .setTitle("Firework " + FireworkNumber.toString())
+                .setMessage("Firework " + FireworkNumber.toString() + " has been launched!")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .show();
+    }
+
+    public void armLauncherFirstMessage() {
+        new AlertDialog.Builder(this)
+                .setTitle("Arm Launcher First")
+                .setMessage("You must arm the launcher before you can launch a firework")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -61,4 +176,5 @@ public class LauncherActivity extends Activity {
             return rootView;
         }
     }
+
 }
